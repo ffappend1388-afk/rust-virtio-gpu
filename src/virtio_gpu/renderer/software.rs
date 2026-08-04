ebe29694f01c82158e1a3c3311e0fbaadce267e2
ffappend1388-afk <ff.append.1388@gmail.com>
2026-08-04 11:14:19 +0330

    refactor: migrate software renderer to RendererError

--- src/virtio_gpu/renderer/software.rs
+++ src/virtio_gpu/renderer/software.rs
@@ -1,6 +1,6 @@
+use super::error::RendererError;
 use super::framebuffer::FrameBuffer;
 use super::renderer::Renderer;
-use crate::virtio_gpu::device::DeviceError;
 use crate::virtio_gpu::resource::Resource;
 
 #[derive(Debug)]
@@ -26,9 +26,11 @@ impl SoftwareRenderer {
     pub fn size(&self) -> (u32, u32) {
         (self.framebuffer.width, self.framebuffer.height)
     }
+
     pub fn framebuffer(&self) -> &FrameBuffer {
         &self.framebuffer
     }
+
     pub fn framebuffer_mut(&mut self) -> &mut FrameBuffer {
         &mut self.framebuffer
     }
@@ -38,21 +40,22 @@ impl Renderer for SoftwareRenderer {
     fn upload(&mut self, data: &[u8]) {
         self.upload(data);
     }
+
     fn framebuffer(&self) -> &FrameBuffer {
         self.framebuffer()
     }
+
     fn framebuffer_mut(&mut self) -> &mut FrameBuffer {
         self.framebuffer_mut()
     }
 
-    fn transfer_resource(&mut self, resource: &mut Resource) -> Result<(), DeviceError> {
+    fn transfer_resource(&mut self, resource: &mut Resource) -> Result<(), RendererError> {
         self.upload(resource.pixels());
-
         Ok(())
     }
-    fn flush_resource(&mut self, resource: &mut Resource) -> Result<(), DeviceError> {
-        self.upload(resource.pixels());
 
+    fn flush_resource(&mut self, resource: &mut Resource) -> Result<(), RendererError> {
+        self.upload(resource.pixels());
         Ok(())
     }
 }
